4029e896f8de59c1a9ad86780e98f6f8519810f7
petrovi4 <40.10.444@gmail.com>
2024-05-24 13:42:46 +0300

    fix settings.py

--- config/settings.py
+++ config/settings.py
@@ -143,8 +143,13 @@ AUTH_USER_MODEL = 'users.User'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
+EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.yandex.ru'
 EMAIL_PORT = 465
 EMAIL_HOST_USER = 's.i.martinoff@yandex.ru'
 EMAIL_HOST_PASSWORD = 'yxmgybeamvpocdne'
 EMAIL_USE_SSL = True
+
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+SERVER_EMAIL = EMAIL_HOST_USER
+
